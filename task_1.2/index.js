def2d62b55e6d0265fef4030e2776101ce357913
Evgeny Ralko <evgeniy.ralko@gmail.com>
2019-12-05 01:05:18 +0300

    refactor task 1.2 to use stream.pipeline

--- task_1.2/index.js
+++ task_1.2/index.js
@@ -1,15 +1,20 @@
+
 const fs = require('fs');
+const {pipeline} = require('stream')
 const csv=require('csvtojson');
 
 const csvFilePath = __dirname + '/csv/node_mentoring_t1_2_input_example.csv';
 const outputFile = __dirname + '/output.txt';
- 
-const writeStream = fs.createWriteStream(outputFile);
-
-csv()
-  .fromFile(csvFilePath)
-  .on('error', (error)=> console.exception(error))
-  .on('data', (chunk)=> writeStream.write(chunk.toString('utf8')));
-
-  writeStream.on('error', (error) => console.exception(error))
   
+pipeline(
+  fs.createReadStream(csvFilePath),
+  csv(),
+  fs.createWriteStream(outputFile),
+  (err) => {
+    if (err) {
+      console.error('Pipeline failed.', err);
+    } else {
+      console.log('Pipeline succeeded.');
+    }
+  }
+);
